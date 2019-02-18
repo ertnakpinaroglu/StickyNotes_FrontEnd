f61f6f95fca9bffedc65d53b7ee44f0e359c127e
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-02-18 19:26:26 +0300

    Case 18: Navcomponent is created was successfully.

---
+++ stickynotes-frontend-project/src/app/Components/NavComponent/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-nav",
+    templateUrl: "./nav.view.html",
+    styleUrls: ["./nav.styles.css"]
+})
+
+export class NavComponent implements OnInit {
+
+    ngOnInit(): void {
+
+
+    }
+
+
+}
+
