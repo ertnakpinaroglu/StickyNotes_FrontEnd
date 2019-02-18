1d11f0e876a90bcd561abb77fdb981f2f753104e
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-02-18 19:39:43 +0300

    Case 20: angular.json configured for use 3rd party softwares

--- stickynotes-frontend-project/src/app/app.module.ts
+++ stickynotes-frontend-project/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavComponent } from "./Components/NavComponent/nav.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
