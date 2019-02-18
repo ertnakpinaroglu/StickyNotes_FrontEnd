b920fe901d0394ce012e8777067b667bda2e6a68
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-02-19 00:50:26 +0300

    Case 24: Reactive form created  for adding board.

--- stickynotes-frontend-project/src/app/Components/BoardComponent/board.component.ts
+++ stickynotes-frontend-project/src/app/Components/BoardComponent/board.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { BoardService } from 'src/app/Services/board.services';
 import { Board } from 'src/app/Models/board';
-
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-board",
@@ -14,13 +14,23 @@ export class BoardComponent implements OnInit {
 
     ngOnInit(): void {
         this.getBoards();
-
+        this.createAddBoardForm();
     }
 
     boardList: Board[] = [];
-    constructor(private boardService: BoardService) {
+    board: Board;
+    boardAddGroup: FormGroup;
+    constructor(private boardService: BoardService, private formBuilder: FormBuilder) {
 
     }
+
+    createAddBoardForm() {
+        this.boardAddGroup = this.formBuilder.group(
+            { color: ["", Validators.required] },
+            { title: ["", Validators.required] }
+        )
+    }
+
 
     getBoards() {
         this.boardService.getBoards().subscribe(boards => {
